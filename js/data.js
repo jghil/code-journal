2b9f1983f483405c6f9706d73c604686b9f81f17
jghil <jayjeonghil94@gmail.com>
2022-07-19 18:40:40 +0000

    completed all tasks to create an entry. #1

--- js/data.js
+++ js/data.js
@@ -6,3 +6,13 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
+
+var previousDataJSON = localStorage.getItem(data);
+if (previousDataJSON !== null) {
+  data = JSON.parse(previousDataJSON);
+}
+
+window.addEventListener('beforeunload', function (e) {
+  var dataJSON = JSON.stringify(data);
+  localStorage.setItem(data, dataJSON);
+});
--- js/main.js
+++ js/main.js
@@ -1,6 +1,19 @@
 var $photoUrlPreview = document.getElementById('placeholderImage');
 var $newPhotoPreview = document.getElementById('photoUrl');
+var $entryForm = document.querySelector('#entry-form');
 
 $newPhotoPreview.addEventListener('input', function (e) {
   $photoUrlPreview.setAttribute('src', e.target.value);
+});
+
+$entryForm.addEventListener('submit', function (e) {
+  event.preventDefault();
+  var newObject = {};
+  newObject.entryId = data.nextEntryId++;
+  newObject.title = $entryForm.elements.title.value;
+  newObject.photoUrl = $entryForm.elements.photoUrl.value;
+  newObject.notes = $entryForm.elements.notes.value;
+  document.getElementById('entry-form').reset();
+  $photoUrlPreview.setAttribute('src', 'images/placeholder-image-square.jpg');
+  data.entries.unshift(newObject);
 });
