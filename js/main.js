1fb1746690b544157166c507936aec6afba2f0fd
jghil <jayjeonghil94@gmail.com>
2022-07-20 01:30:00 +0000

    fixed li and ul html, styled to work both on both mobile and desktop screens, defined function to return a dom tree, used a loop to create a dom tree for each journal entry and append it ot the page, removed example html entries. #2

--- js/main.js
+++ js/main.js
@@ -17,3 +17,48 @@ $entryForm.addEventListener('submit', function (e) {
   $photoUrlPreview.setAttribute('src', 'images/placeholder-image-square.jpg');
   data.entries.unshift(newObject);
 });
+
+function renderObject(data) {
+  var liWrapper = document.createElement('li');
+  liWrapper.setAttribute('class', 'no-bullets');
+  liWrapper.setAttribute('class', 'li-space');
+
+  var divRow = document.createElement('div');
+  divRow.setAttribute('class', 'row');
+
+  var columnHalf = document.createElement('div');
+  columnHalf.setAttribute('class', 'column-half');
+
+  var imgTag = document.createElement('img');
+  imgTag.setAttribute('src', data.photoUrl);
+  imgTag.setAttribute('class', 'images');
+
+  var columnHalfTwo = document.createElement('div');
+  columnHalfTwo.setAttribute('class', 'column-half');
+
+  var entryTitle = document.createElement('h2');
+  entryTitle.setAttribute('class', 'no-margin');
+  entryTitle.textContent = data.title;
+
+  var entryContent = document.createElement('p');
+  entryContent.setAttribute('class', 'no-margin');
+
+  entryContent.textContent = data.notes;
+
+  liWrapper.appendChild(divRow);
+  divRow.appendChild(columnHalf);
+  columnHalf.appendChild(imgTag);
+  divRow.appendChild(columnHalfTwo);
+  columnHalfTwo.appendChild(entryTitle);
+  columnHalfTwo.appendChild(entryContent);
+
+  return liWrapper;
+}
+
+document.addEventListener('DOMContentLoaded', function (e) {
+  var ulList = document.querySelector('ul');
+  for (var i = 0; i < data.entries.length; i++) {
+    var journalEntry = renderObject(data.entries[i]);
+    ulList.appendChild(journalEntry);
+  }
+});
